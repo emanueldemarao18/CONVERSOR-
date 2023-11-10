96051eead4bf73969ae6dd56715f8360e93b8578
Emanuel _de_Marao <emanueldemarao20@gmail.com>
2023-11-10 15:19:29 +0000

    Adicionei o IVA no levantamento

--- script.js
+++ script.js
@@ -5,11 +5,12 @@ function calcularConversao() {
     var descontoTotal = desconto2Percent + desconto14Percent;
     var totalComDescontos = valor - descontoTotal;
     var totalpercent = (totalComDescontos * 0.03);
-    var totalEurosNoATM = totalComDescontos - totalpercent;
+    var totalpercentfinal = (totalpercent*0.14);
+    var totalEurosNoATM = totalComDescontos - totalpercentfinal;
     var cambioAtual = parseFloat(document.getElementById('cambioAtual').value);
     var resultadoFinal = totalEurosNoATM / cambioAtual;
 
     document.getElementById('resultados').innerHTML = "<h3>Resultados:</h3>";
-    document.getElementById('resultados').innerHTML += "<p>VALOR NO KAMBA BAI: " + totalComDescontos.toFixed(2) + "</p>";
-    document.getElementById('resultados').innerHTML += "<p>VALOR APÓS LEVANTAMENTO NO ATM: " + resultadoFinal.toFixed(2) + "</p>";
+    document.getElementById('resultados').innerHTML += "<p>VALOR NO KAMBA BAI EM KWANZA: " + totalComDescontos.toFixed(2) + "</p>";
+    document.getElementById('resultados').innerHTML += "<p>VALOR DISPONÍVEL PARA LEVANTAMENTO NO ATM EM EURO: " + resultadoFinal.toFixed(2) + "</p>";
 }
